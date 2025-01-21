8379185201df9ee702246b9da8eb89cbe9324f71
Jorgegiral <jorgegiraldo758@gmail.com>
2025-01-21 13:35:26 +0100

    cambios_clase

--- Prototype_Time_Game/Assets/Time_Game/Scripts/OpenObject.cs
+++ Prototype_Time_Game/Assets/Time_Game/Scripts/OpenObject.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class OpenObject : MonoBehaviour
 {
     public string requiredItemID;
     public Inventory inventory;
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OpenDoor();
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -15,12 +23,17 @@ public class OpenObject : MonoBehaviour
             OpenDoor();
         }
     }
+    //void Update()
+    //{
+    //if (Input.GetKeyDown(KeyCode.E) )
+    //{
+    //OpenDoor();
+    //       }
     void OpenDoor()
     {
         if (inventory.HasItem(requiredItemID))
         {
             gameObject.SetActive(false);
-            inventory.RemoveItem(requiredItemID);
         }
     }
 }
